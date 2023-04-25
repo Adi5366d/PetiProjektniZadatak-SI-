99395bd62015cfe9a7897f158b1b5e736cdfb4fc
Adi <84505646+Adi5366d@users.noreply.github.com>
2023-04-25 12:21:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,65 @@
-const apiKey = 'dec639d494b26e2d863c9e80aa1f5b63';
-
-// get references to the HTML elements that we need to update
-const cityName = document.getElementById('city-name');
-const temperature = document.getElementById('temperature');
-const weatherDescription = document.getElementById('weather-description');
-const weatherIcon = document.getElementById('weather-icon');
-const visibilityData = document.getElementById('visibility-data');
-
-// function that fetches weather data and updates the HTML content
-async function fetchWeatherData(city) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=hr&appid=${apiKey}`;
-
-  const response = await fetch(url);
-  const data = await response.json();
-
-  console.log(data); // add this line to check what data is being returned
-
-  cityName.textContent = data.name;
-
-  temperature.textContent = `${data.main.temp.toFixed()} °C`;
-  weatherDescription.textContent = data.weather[0].description;
-  visibilityData.textContent = `${data.visibility} m`;
-
-  const iconCode = data.weather[0].icon;
-  const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
-  weatherIcon.src = iconUrl;
-}
-
-// get the form and add a submit event listener
-const form = document.getElementById('weather-form');
-form.addEventListener('submit', event => {
-  event.preventDefault();
-
-  const input = document.getElementById('city-input');
-  const city = input.value;
-
-  fetchWeatherData(city);
-  input.value = '';
-});
+const firebaseConfig = {
+    apiKey: "AIzaSyA7e0vLqwFMiMluQ6-FIIsl7NXDXv0xjo4",
+    authDomain: "projekatadi.firebaseapp.com",
+    databaseURL: "https://projekatadi-default-rtdb.firebaseio.com",
+    projectId: "projekatadi",
+    storageBucket: "projekatadi.appspot.com",
+    messagingSenderId: "52269012864",
+    appId: "1:52269012864:web:7fe34b1a19590cdf119365",
+    measurementId: "G-PVYB81J1V6"
+  };
+  
+    
+    
+    firebase.initializeApp(firebaseConfig);
+    
+    // Kolekcija referentnih poruka
+    var messagesRef = firebase.database().ref('messages');
+    
+    // submitanje forme
+    document.getElementById('contactForm').addEventListener('submit', submitForm);
+    
+    // Submit forma
+    function submitForm(e){
+      e.preventDefault();
+    
+      // vrijednosti
+      var name = getInputVal('name');
+      var company = getInputVal('company');
+      var email = getInputVal('email');
+      var phone = getInputVal('phone');
+      var message = getInputVal('message');
+    
+      // Spasi poruku
+      saveMessage(name, company, email, phone, message);
+    
+      // obavjestenje
+      document.querySelector('.alert').style.display = 'block';
+    
+      // nakon 3s sakrij obavjestenje
+      setTimeout(function(){
+        document.querySelector('.alert').style.display = 'none';
+      },3000);
+    
+      // Clear screen
+      document.getElementById('contactForm').reset();
+    }
+    
+    // funkcija za dobijanje vrijednosti forme
+    function getInputVal(id){
+      return document.getElementById(id).value;
+    }
+    
+    // spasi prouku za firebase
+    function saveMessage(name, company, email, phone, message){
+      var newMessageRef = messagesRef.push();
+      newMessageRef.set({
+        name: name,
+        company:company,
+        email:email,
+        phone:phone,
+        message:message
+      });
+    }
 
 
